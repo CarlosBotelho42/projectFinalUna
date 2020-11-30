a0ca3f7319056f5ecb6652ace3f8e67659566fa4
CarlosBotelho42 <carlospiece55@gmail.com>
2020-11-30 20:08:38 -0300

    projeto do desespero(vai da certo)

---
+++ src/main/java/com/finalProject/projectUna/ProjectUnaApplication.java
@@ -0,0 +1,13 @@
+package com.finalProject.projectUna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalProject/projectUna/ProjectUnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProject.projectUna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
